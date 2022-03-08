99a174ddc67f030735e3286621fdb8dbfd603add
endoresur <mlhv2lv@gmail.com>
2022-03-08 14:47:50 +0500

    Upgrated containers

--- src/styles/Containers.js
+++ src/styles/Containers.js
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 const Container = styled.div`
   position: relative;
-  //justify-content: center;
-  align-items: center;
-  
-  margin: auto 130px auto 130px;
-  
+  justify-content: ${props => props.jc || ''};
+  align-items: ${props => props.ai || 'center'};
+
+  margin: 
+    ${props => props.mt || 'auto'}
+    ${props => props.mr || '130px'}
+    ${props => props.mb || 'auto'}
+    ${props => props.ml || '130px'}
 `
 
 const FlexContainer = styled(Container)`
@@ -17,7 +20,7 @@ const FlexContainer = styled(Container)`
 
 const HeaderContainer = styled.header`
   height: 85px;
-  margin-bottom: 30px;
+  margin-bottom: 60px;
   
   font-style: normal;
   font-weight: 500;
@@ -28,4 +31,11 @@ const HeaderContainer = styled.header`
   box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
 `
 
-export {Container, FlexContainer, HeaderContainer};
+const TextContainer = styled.div`
+  font-style: ${props => props.fStyle || ''};
+  font-weight: ${props => props.fWeight || ''};
+  font-size: ${props => props.fSize || ''};
+  line-height: ${props => props.lh || ''};
+`
+
+export {Container, FlexContainer, HeaderContainer, TextContainer};
